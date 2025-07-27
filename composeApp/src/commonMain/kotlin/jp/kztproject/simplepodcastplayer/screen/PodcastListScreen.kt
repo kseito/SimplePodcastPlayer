49d30e55694cee36f2c59f4e7009539e61a85b85
kseito <torinokonoko@gmail.com>
2025-07-27 14:41:52 +0900

    Support edge-to-edge

--- composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/screen/PodcastListScreen.kt
+++ composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/screen/PodcastListScreen.kt
@@ -1,13 +1,25 @@
 package jp.kztproject.simplepodcastplayer.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PodcastListScreen(onNavigateToSearch: () -> Unit) {
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
+            .padding(16.dp)
+    ) {
         Text("Podcast List")
         Button(onClick = onNavigateToSearch) {
             Text("Go to Search")
