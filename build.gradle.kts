9f699b63bfd5cb9952003c86516dfa3bb0f1fc54
kseito <torinokonoko@gmail.com>
2025-07-27 15:18:44 +0900

    Add detekt definition

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,25 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.kotlinMultiplatform) apply false
+}
+
+detekt {
+    source.setFrom(
+        "composeApp/src/commonMain/kotlin",
+        "composeApp/src/androidMain/kotlin", 
+        "composeApp/src/iosMain/kotlin"
+    )
+    config.setFrom("$rootDir/detekt-config.yml")
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
 }
