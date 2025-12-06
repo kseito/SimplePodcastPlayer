60bb656ed1b71f79fed4fcc2516c88c67332cbc3
kseito <torinokonoko@gmail.com>
2025-12-06 19:55:25 +0900

    Add HTTP timeout and logging to AppleSearchApiClient

--- composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/data/AppleSearchApiClient.kt
+++ composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/data/AppleSearchApiClient.kt
@@ -2,7 +2,11 @@ package jp.kztproject.simplepodcastplayer.data
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
@@ -25,6 +29,21 @@ class AppleSearchApiClient {
                 },
                 contentType = ContentType.Any,
             )
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15_000 // 15秒
+            connectTimeoutMillis = 15_000 // 15秒
+            socketTimeoutMillis = 15_000 // 15秒
+        }
+
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    println("Ktor: $message")
+                }
+            }
+            level = LogLevel.ALL // すべてのログを出力
         }
     }
 
