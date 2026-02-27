a949bd1e636b8b6669c959510d326ad88cb8a7fc
kseito <torinokonoko@gmail.com>
2026-02-28 07:36:19 +0900

    fix: simplify PodcastDetailScreen navigation by directly passing podcastId

--- composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/App.kt
+++ composeApp/src/commonMain/kotlin/jp/kztproject/simplepodcastplayer/App.kt
@@ -13,14 +13,10 @@ import jp.kztproject.simplepodcastplayer.di.appModule
 import jp.kztproject.simplepodcastplayer.screen.PlayerScreen
 import jp.kztproject.simplepodcastplayer.screen.PodcastDetailScreen
 import jp.kztproject.simplepodcastplayer.screen.PodcastListScreen
-import jp.kztproject.simplepodcastplayer.screen.PodcastListViewModel
 import jp.kztproject.simplepodcastplayer.screen.PodcastSearchScreen
 import jp.kztproject.simplepodcastplayer.screen.rememberPlayerViewModel
-import jp.kztproject.simplepodcastplayer.data.repository.PodcastRepository
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
-import org.koin.compose.koinInject
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 @Preview
@@ -68,23 +64,16 @@ fun App() {
                     }
                 }
                 composable("list_detail") {
-                    // This screen is accessed from PodcastListScreen
-                    // We need to get the podcast from the database using selectedPodcastId
                     selectedPodcastId.value?.let { podcastId ->
-                        // Get the podcast from the view model
-                        val podcastRepository: PodcastRepository = koinInject()
-                        val listViewModel: PodcastListViewModel = viewModel { PodcastListViewModel(podcastRepository) }
-                        listViewModel.getPodcastById(podcastId)?.let { podcast ->
-                            PodcastDetailScreen(
-                                podcast = podcast,
-                                onNavigateBack = { navController.popBackStack() },
-                                onNavigateToPlayer = { episode, podcastData ->
-                                    selectedEpisode.value = episode
-                                    selectedEpisodePodcast.value = podcastData
-                                    navController.navigate("player")
-                                },
-                            )
-                        }
+                        PodcastDetailScreen(
+                            podcastId = podcastId,
+                            onNavigateBack = { navController.popBackStack() },
+                            onNavigateToPlayer = { episode, podcastData ->
+                                selectedEpisode.value = episode
+                                selectedEpisodePodcast.value = podcastData
+                                navController.navigate("player")
+                            },
+                        )
                     }
                 }
                 composable("player") {
