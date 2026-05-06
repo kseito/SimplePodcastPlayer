83eebf4bd19a0d67ad21027ee26f88da7faa4b08
kseito <torinokonoko@gmail.com>
2026-05-06 10:22:14 +0900

    fix: initialize logging in main view controller for better debugging

--- composeApp/src/iosMain/kotlin/jp/kztproject/simplepodcastplayer/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/jp/kztproject/simplepodcastplayer/MainViewController.kt
@@ -1,8 +1,13 @@
 package jp.kztproject.simplepodcastplayer
 
 import androidx.compose.ui.window.ComposeUIViewController
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import platform.UIKit.UIViewController
 
-fun mainViewController(): UIViewController = ComposeUIViewController {
-    App()
+fun mainViewController(): UIViewController {
+    Napier.base(DebugAntilog())
+    return ComposeUIViewController {
+        App()
+    }
 }
